790fc5bc6a2053c5e90da2c25e1614b5e02edcc7
seonghyeon <shp35079@naver.com>
2021-07-14 23:24:59 +0900

    [ADD] globalstyles

--- src/Globalstyles.js
+++ src/Globalstyles.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import { default as BlackHansSansFont } from './assets/fonts/BlackHanSans-Regular.ttf';
 import { default as NotoSansKRFont } from './assets/fonts/NotoSansCJKkr-Regular.otf';
+import { default as DoHyeonFont } from './assets/fonts/DoHyeon-Regular.ttf';
 
 const GlobalStyle = createGlobalStyle`
   *, *::before, *::after {
@@ -17,9 +18,23 @@ const GlobalStyle = createGlobalStyle`
     src: url(${NotoSansKRFont});
   }
 
+  @font-face {
+    font-family: 'DoHyeon';
+    src: url(${DoHyeonFont});
+  }
+
   body {
     margin: 0;
     padding: 0;
+  }
+
+  p, h1, h2, h3 {
+    margin: 0;
+    padding: 0;
+  }
+
+  span {
+    cursor: default;
   }
 `;
 
