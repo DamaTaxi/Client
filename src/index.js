37bd3df94b6c9fd22a6bde52d37940de5425d05b
seonghyeon <shp35079@naver.com>
2021-07-14 23:24:46 +0900

    [ADD] router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import GlobalStyle from './Globalstyles';
 
 ReactDOM.render(
-  <>
+  <BrowserRouter>
     <GlobalStyle />
     <App />
-  </>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
