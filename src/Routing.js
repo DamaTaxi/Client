49f111267ce4ff64c85f51f7ffc6be030e6cce91
JaeHyun <soehd0889@naver.com>
2021-07-22 21:47:12 +0900

    [FEAT] <src/Routing.js> add Routing code

---
+++ src/Routing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+/* import Home from './Home'; */
+
+const Routing = () => {
+  return (
+    <>
+      <Switch>
+        {/* <Route path="/" exact component={Home} /> */}
+      </Switch>
+    </>
+  );
+};
+
+export default Routing;
