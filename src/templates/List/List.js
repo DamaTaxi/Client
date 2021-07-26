37c69d6a52442ef67d3cdb12bb09bc21f3ade8a2
seonghyeon <shp35079@naver.com>
2021-07-26 17:26:34 +0900

    [CREATE] <src/templates/list> create

---
+++ src/templates/List/List.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import * as S from './styles';
+
+const List = ({ title, list, pageNum }) => {
+  return (
+    <S.Wrapper>
+      <S.Title>{title}</S.Title>
+      <S.ListContainer>
+        {list.map((ele) => (
+          <S.List key={ele.id}>
+            <span>{ele.id}</span>
+            <span>{ele.title}</span>
+          </S.List>
+        ))}
+        <S.PageNum>
+          <span>
+            <S.Left />
+            <S.Left />
+          </span>
+          <span>
+            <S.Left />
+          </span>
+          {pageNum.map((ele) => (
+            <span>{ele}</span>
+          ))}
+          <span>
+            <S.Right />
+          </span>
+          <span>
+            <S.Right />
+            <S.Right />
+          </span>
+        </S.PageNum>
+      </S.ListContainer>
+    </S.Wrapper>
+  );
+};
+
+export default List;
