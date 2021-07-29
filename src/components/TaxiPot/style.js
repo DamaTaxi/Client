aa63401179a95a4bbbd7f3cd620b8f8463069f7f
JaeHyun <soehd0889@naver.com>
2021-07-29 20:59:43 +0900

    [FEAT] <src/assets/images/TaxiPot> add image

---
+++ src/components/TaxiPot/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Header = styled.div`
+  width: 100%;
+  height: 127px;
+  img:first-child {
+    margin: 20px 0 8px 39px;
+  }
+`;
+
+export const TaxiPotSlideContainer = styled.div`
+  width: 100%;
+  height: 587px;
+`;
