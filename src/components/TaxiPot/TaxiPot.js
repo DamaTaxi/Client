ba792b58bd9f97cc3e9b774640390d582aa2d41c
JaeHyun <soehd0889@naver.com>
2021-07-30 11:18:18 +0900

    [FEAT] <src/components/TaxiPot> add slider code

--- src/components/TaxiPot/TaxiPot.js
+++ src/components/TaxiPot/TaxiPot.js
@@ -2,6 +2,7 @@ import React from 'react';
 import * as S from './style';
 import headerLogoYellow from '../../assets/images/TaxiPot/header-logo-yellow.png';
 import headerLineYellow from '../../assets/images/TaxiPot/header-line-yellow.png';
+import TaxiPotSlider from './TaxiPotSlider/TaxiPotSlider';
 
 const TaxiPot = () => {
   return (
@@ -10,7 +11,9 @@ const TaxiPot = () => {
         <img src={headerLogoYellow} alt="header-logo-yellow" />
         <img src={headerLineYellow} alt="header-line-yellow" />
       </S.Header>
-      <S.TaxiPotSlideContainer></S.TaxiPotSlideContainer>
+      <S.TaxiPotSlideContainer>
+        <TaxiPotSlider> </TaxiPotSlider>
+      </S.TaxiPotSlideContainer>
     </>
   );
 };
