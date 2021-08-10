c33e70552fb4749217fda490c3d1a81ad08f27bc
seonghyeon <shp35079@naver.com>
2021-08-11 00:52:40 +0900

    [FEAT] <src/Modal/ListModal> publising complete

--- src/Modal/ListModal/ListModal.js
+++ src/Modal/ListModal/ListModal.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import * as S from './styles';
 
-const ListModal = ({ isShowModal, closeModal }) => {
+const ListModal = ({ ModalContent, isShowModal, closeModal }) => {
   return (
     isShowModal && (
       <>
         <S.Background onClick={closeModal} />
-        <S.ModalContainer></S.ModalContainer>
+        <S.ModalContainer>
+          <span>택시팟이 안 만들어져요 ㅠㅠ</span>
+          <S.ContentContainer>
+            입력칸 다 채우고 생성 버튼 눌렀는데도 제가 만든 팟이 안 보이네요 ㅠㅠ 오류 고쳐주세요!! 저 제주도
+            가야합니다.
+          </S.ContentContainer>
+          <S.ButtonContainer>
+            <button onClick={closeModal}>확인</button>
+            <button>삭제</button>
+          </S.ButtonContainer>
+        </S.ModalContainer>
       </>
     )
   );
--- src/Modal/ListModal/styles.js
+++ src/Modal/ListModal/styles.js
@@ -14,12 +14,57 @@ export const ModalContainer = styled.div`
   position: fixed;
   left: 50%;
   top: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   transform: translate(-50%, -50%);
   max-height: 80%;
   width: 60%;
-  height: 70%;
+  height: 75%;
   padding: 40px;
   background: #fff;
   border-radius: 30px;
   z-index: 2;
+
+  & span {
+    margin-bottom: 40px;
+    font-size: 40px;
+    font-family: 'NotoSansKR';
+    font-weight: bold;
+  }
+`;
+
+export const ContentContainer = styled.div`
+  width: 100%;
+  height: 500px;
+  margin-bottom: 25px;
+  padding: 30px;
+  border: 3px solid #8d8d8d;
+  border-radius: 10px;
+  font-size: 22px;
+  font-family: 'NotoSansCJK';
+  font-weight: 600;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 30px;
+
+  & button {
+    width: 125px;
+    height: 45px;
+    border: 1px solid #707070;
+    border-radius: 20px;
+    font-size: 16px;
+    font-family: 'NotoSansCJK';
+    font-weight: 600;
+    cursor: pointer;
+  }
+
+  & button:first-child {
+    border: none;
+    background-color: #ffc044;
+  }
 `;
