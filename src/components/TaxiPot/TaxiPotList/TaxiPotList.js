b452f05ef7a18b4d0920129c1b930dd3926aaef9
JaeHyun <soehd0889@naver.com>
2021-08-17 22:54:11 +0900

    [FEAT] <src/components/TaxiPot/TaxiPotList /> add publishing code

--- src/components/TaxiPot/TaxiPotList/TaxiPotList.js
+++ src/components/TaxiPot/TaxiPotList/TaxiPotList.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import * as S from './style';
+import { Link } from 'react-router-dom';
 
 const TaxiPotList = () => {
-  return <h1>hello i am list</h1>
-}
+  return (
+    <S.taxiPotListWrapper>
+      <>
+        <S.listHeader>
+          <h1>택시 팟 리스트</h1>
+          <Link to="#">
+            <p>+</p>
+            <p>택시 팟 만들기</p>
+          </Link>
+        </S.listHeader>
+      </>
+      <>
+        <S.listContainer>
+          <Link>
+            <S.taxiPotList>
+              <div>
+                <div className=""></div>
+                <div className=""></div>
+              </div>
+              <div className="kakaoMap"></div>
+            </S.taxiPotList>
+          </Link>
+          <Link>
+            <S.taxiPotList></S.taxiPotList>
+          </Link>
+        </S.listContainer>
+      </>
+    </S.taxiPotListWrapper>
+  );
+};
 
-export default TaxiPotList
+export default TaxiPotList;
---
+++ src/components/TaxiPot/TaxiPotList/style.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+export const taxiPotListWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const listHeader = styled.div`
+  margin-top: 35px;
+  width: 1470px;
+  height: 65px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  h1 {
+    font-family: 'NotoSansKR';
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  a {
+    display: flex;
+    align-items: center;
+    //택시팟 만들기 버튼(Link)
+    background-color: #ffc044;
+    width: 177px;
+    height: 50px;
+    border-radius: 50px;
+    color: white;
+    text-decoration: none;
+    justify-content: center;
+    p {
+      font-family: 'NotoSansKR';
+      font-weight: bold;
+    }
+    p:first-child {
+      font-size: 44px;
+      padding: 4px 8px 0 0;
+    }
+    p:nth-child(2) {
+      font-size: 20px;
+    }
+  }
+`;
+
+export const listContainer = styled.div`
+  width: 1487px;
+  height: auto;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  a:nth-child(n + 2) {
+    margin-top: 50px;
+  }
+`;
+
+export const taxiPotList = styled.div`
+  width: 1457px;
+  height: 172px;
+  border: solid #ffc044 5px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+`;
