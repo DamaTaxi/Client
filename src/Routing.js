8135eb52c90c9406b6e6f39e7f76aa403cde004b
JaeHyun <soehd0889@naver.com>
2021-08-18 19:27:28 +0900

    [FEAT] <src/Routing.js> add my-taxi-pot

--- src/Routing.js
+++ src/Routing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import TaxiPot from './components/TaxiPot/TaxiPot';
+import MyTaxiPot from './components/TaxiPotDetailView/MyTaxiPot/MyTaxiPot';
 
 
 const Routing = () => {
@@ -8,6 +9,7 @@ const Routing = () => {
     <>
       <Switch>
         <Route path="/taxi-pot" exact component={TaxiPot} /> 
+        <Route path="/my-taxi-pot" exact component={MyTaxiPot} /> 
       </Switch>
     </>
   );
