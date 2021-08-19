a7cfecc36a5286907589090cff112bbacb68ada1
JaeHyun <soehd0889@naver.com>
2021-08-19 22:59:11 +0900

    [MODIFY] <src/templates/GraphContainer> modify styled-components props

--- src/templates/GraphContainer/style.js
+++ src/templates/GraphContainer/style.js
@@ -5,15 +5,18 @@ export const GraphContainer = styled.div`
   height: 26px;
   display: flex;
   align-items: center;
+  p:first-child {
+    margin-right: 8px;
+  }
   .yellowBorderBox {
     position: relative;
-    width: ${(props) => (props.width ? `${props.width}px` : '118px')};
-    height: ${(props) => (props.height ? `${props.height}px` : '20px')};
+    width: ${(props) => `${props.width}px`};
+    height: ${(props) => `${props.height}px`};
     border: #ffc044 1px solid;
     p {
       position: absolute;
       text-align: center;
-      left: ${(props) => (props.left ? `${props.left}%` : `42.08%`)};
+      left: ${(props) => `${props.left}%`};
       z-index: 10;
       font-size: 1rem;
     }
@@ -22,7 +25,7 @@ export const GraphContainer = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    width: ${(props) => (props.filled ? `${props.filled}%` : `0`)};
+    width: ${(props) => `${props.filled}%`};
     height: 100%;
     background-color: #ffc044;
     text-align: center;
