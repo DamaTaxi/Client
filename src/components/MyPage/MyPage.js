4f30a311165ffe4f7c8c630b9ef8da79bbd39b58
seonghyeon <shp35079@naver.com>
2021-08-24 23:16:17 +0900

    [ADD] <src/components/MyPage> MyPage Publishing

--- src/components/MyPage/MyPage.js
+++ src/components/MyPage/MyPage.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import * as S from './styles';
+import Modify from '../../assets/images/modify.svg';
 
 const MyPage = () => {
-  return <div></div>;
+  return (
+    <S.Wrapper>
+      <S.Title>2101 권민정</S.Title>
+      <S.Content>
+        <S.NumberBox>
+          <div>전화번호</div>
+          <S.UserInfo>
+            <span>010-2809-3338</span>
+            <S.ModifyBox>
+              <img src={Modify} alt=""></img>
+              <span>수정</span>
+            </S.ModifyBox>
+          </S.UserInfo>
+        </S.NumberBox>
+        <S.EmailBox>
+          <div>이메일</div>
+          <S.UserInfo>
+            <span>kodohyeon71@gmail.com</span>
+            <S.ModifyBox>
+              <img src={Modify} alt=""></img>
+              <span>수정</span>
+            </S.ModifyBox>
+          </S.UserInfo>
+        </S.EmailBox>
+        <S.PlaceBox>
+          <div>자주 가는 곳</div>
+          <div></div>
+        </S.PlaceBox>
+        <S.PotBox>
+          <div>현재 예약된 팟</div>
+          <S.PotInfo>
+            <span>둔산동 꿀잼동전노래연습장</span>
+            <p>대상자: 2학년</p>
+            <p>km: 9.8km</p>
+            <p>예상가격: 10,000원</p>
+            <p>현재 인원 수: 2/4</p>
+          </S.PotInfo>
+        </S.PotBox>
+      </S.Content>
+    </S.Wrapper>
+  );
 };
 
 export default MyPage;
--- src/components/MyPage/styles.js
+++ src/components/MyPage/styles.js
@@ -1,1 +1,94 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 350px;
+`;
+
+export const Title = styled.span`
+  margin-bottom: 120px;
+  font-size: 60px;
+  font-family: 'NotoSansKR';
+  font-weight: bold;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  & div {
+    width: 1400px;
+    display: flex;
+  }
+
+  & div div:first-child {
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px 0 0 20px;
+    background-color: #ffc044;
+    color: white;
+    font-size: 36px;
+    font-family: 'NotoSansKR';
+    font-weight: bold;
+  }
+`;
+
+export const UserInfo = styled.div`
+  width: 70%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-right: 30px;
+  padding-left: 80px;
+  font-size: 36px;
+  font-family: 'DoHyeon';
+  font-weight: 400;
+  border: 3px solid #ffc044;
+`;
+
+export const NumberBox = styled.div`
+  height: 70px;
+`;
+
+export const EmailBox = styled.div`
+  height: 70px;
+`;
+
+export const PlaceBox = styled.div`
+  height: 120px;
+`;
+
+export const PotBox = styled.div`
+  height: 340px;
+`;
+
+export const ModifyBox = styled.span`
+  cursor: pointer;
+  & span {
+    font-size: 16px;
+  }
+`;
+
+export const PotInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border: 3px solid #ffc044;
+  padding-left: 80px;
+  font-family: 'DoHyeon';
+  font-weight: 400;
+
+  & span {
+    font-size: 36px;
+    margin-bottom: 100px;
+  }
+
+  & p {
+    font-size: 30px;
+  }
+`;
