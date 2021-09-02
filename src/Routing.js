5b8b8ce9bfc568882ab0f58b9a0650eff9d78bfc
JaeHyun <soehd0889@naver.com>
2021-09-02 23:49:30 +0900

    [ADD] add routing

--- src/Routing.js
+++ src/Routing.js
@@ -3,14 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import TaxiPot from './components/TaxiPot/TaxiPot';
 import MyTaxiPot from './components/TaxiPotDetailView/MyTaxiPot/MyTaxiPot';
 import ModifyTaxiPot from './components/TaxiPotDetailView/ModifyTaixPot/ModifyTaxiPot';
+import SearchKakaoMap from './components/TaxiPotDetailView/SearchKakaoMap/SearchKakaoMap';
 
 const Routing = () => {
   return (
     <>
       <Switch>
-        <Route path="/taxi-pot" exact component={TaxiPot} /> 
-        <Route path="/my-taxi-pot" exact component={MyTaxiPot} /> 
-        <Route path="/modify-my-taxi-pot" exact component={ModifyTaxiPot} /> 
+        <Route path="/taxi-pot" exact component={TaxiPot} />
+        <Route path="/my-taxi-pot" exact component={MyTaxiPot} />
+        <Route path="/modify-my-taxi-pot" exact component={ModifyTaxiPot} />
+        <Route path="/search-kakao" exact component={SearchKakaoMap} />
       </Switch>
     </>
   );
