b6b32a3cd63bf17abb60069fc01383539190af6b
JaeHyun <soehd0889@naver.com>
2021-09-03 10:21:10 +0900

    [MODIFY] <src/~/SearchKakaoMap.js>  modify html

--- src/components/TaxiPotDetailView/SearchKakaoMap/SearchKakaoMap.js
+++ src/components/TaxiPotDetailView/SearchKakaoMap/SearchKakaoMap.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import { Link } from 'react-router-dom';
+import SearchForm from './SearchForm/SearchForm';
+import OptionWrapper from '../../../templates/OptionWrapper/OptionWrapper';
 
 const SearchKakaoMap = () => {
+  const [searchContent, setSearchContent] = useState('');
+  const onSubmitEvent = (e) => {
+    e.preventDefault();
+    alert(searchContent);
+  };
+  const onChangeSearch = (e) => {
+    const { value } = e.target;
+    setSearchContent(value);
+  };
+
   return (
     <S.SearchKakaoMapWrapper>
       <Link to="/" id="taxiPotLogoA">
@@ -10,19 +22,15 @@ const SearchKakaoMap = () => {
       </Link>
       <S.MainArticle>
         <S.MainSection>
-          <S.SearchListWrapper></S.SearchListWrapper>
+          <S.SearchListWrapper>
+            <SearchForm onSubmitEvent={onSubmitEvent} onChangeSearch={onChangeSearch}></SearchForm>
+          </S.SearchListWrapper>
           <S.KakaoMapWrapper>
             <div id="designBox">도착지 검색</div>
             <div></div>
           </S.KakaoMapWrapper>
         </S.MainSection>
-        <S.OptionWrapper>
-          <p>정확한 도착지를 찾아 선택해주세요!</p>
-          <input type="submit" form="taxiPotForm" value="선택" />
-          <Link to="/taxi-pot">
-            <button>취소</button>
-          </Link>
-        </S.OptionWrapper>
+        <OptionWrapper contents="정확한 도착지를 찾아 선택해주세요!" buttonName="선택" cansleName="취소" link="#" />
       </S.MainArticle>
       <S.BackgroundColor />
     </S.SearchKakaoMapWrapper>
