237f768f27b64f935e58179b8bc4deb8836d417c
JaeHyun <soehd0889@naver.com>
2021-09-03 09:42:29 +0900

    [DELETE] <src/~/SearchKakoMap> delete style code

--- src/components/TaxiPotDetailView/SearchKakaoMap/style.js
+++ src/components/TaxiPotDetailView/SearchKakaoMap/style.js
@@ -77,39 +77,3 @@ export const KakaoMapWrapper = styled.div`
     border-top-right-radius: 0;
   }
 `;
-
-export const OptionWrapper = styled.div`
-  width: 100%;
-  height: 80px;
-  background-color: #f5efe7;
-  border-radius: 26px;
-  margin-top: 21px;
-  display: flex;
-  align-items: center;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
-  p {
-    font-family: 'DoHyeon';
-    font-size: 28px;
-    color: #9a9a9a;
-    margin-left: 57px;
-  }
-  input[type='submit'],
-  button {
-    font-family: 'NotoSansCJK';
-    font-size: 23px;
-    font-weight: 500;
-    width: 188px;
-    height: 60px;
-    border-radius: 20px;
-    border: none;
-    cursor: pointer;
-  }
-  input[type='submit'] {
-    margin-left: 750px;
-    background-color: #ffffff;
-  }
-  a > button {
-    background-color: #ffc044;
-    margin-left: 23px;
-  }
-`;
