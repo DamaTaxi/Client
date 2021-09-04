49f63f3b65179b5f0c63f9e9f002447b63849716
JaeHyun <soehd0889@naver.com>
2021-09-04 09:17:30 +0900

    [FEAT] <src\~\SearchForm.js> add input value

--- src/components/TaxiPotDetailView/SearchKakaoMap/SearchForm/SearchForm.js
+++ src/components/TaxiPotDetailView/SearchKakaoMap/SearchForm/SearchForm.js
@@ -2,12 +2,12 @@ import React from 'react';
 import * as S from './style';
 
 const SearchForm = (props) => {
-  const { onSubmitEvent, onChangeSearch } = props;
+  const { onSubmitEvent, onChangeSearch,value } = props;
   return (
     <S.SearchForm onSubmit={onSubmitEvent}>
       <h1>도착지(제목)</h1>
       <div className="searchWrapper">
-        <input className="searchBar" type="text" placeholder="검색어를 입력해 주세요." onChange={onChangeSearch} />
+        <input className="searchBar" type="text" placeholder="검색어를 입력해 주세요." onChange={onChangeSearch} value={value} />
         <div id="buttonWrapper">
           <input type="submit" value="검색" />
         </div>
