b618383cbdb538394dfe935a84ecddc3e8920bbe
JaeHyun <soehd0889@naver.com>
2021-09-04 09:19:47 +0900

    [ADD] <src/components/~/SearchListMap.js> 카카오 키워드 검색기능 추가

---
+++ src/components/TaxiPotDetailView/SearchKakaoMap/SearchListMap/SearchListMap.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import * as S from './style';
+import SearchForm from '../SearchForm/SearchForm';
+
+const { kakao } = window;
+
+const SearchListMap = () => {
+  const [onChagneSearchContent, setOnChagneSearchContent] = useState('');
+  const [onSubmitSearchContent, setOnSubmitSearchContent] = useState('');
+  const [clickList, setClickList] = useState([]);
+  const [isSearch, setIsSearch] = useState(false);
+  const [places, setPlaces] = useState([]);
+
+  const onSubmitEvent = (e) => {
+    e.preventDefault();
+    setOnSubmitSearchContent(onChagneSearchContent);
+    setOnChagneSearchContent('');
+    setIsSearch(true);
+  };
+  const onChangeSearch = (e) => {
+    const { value } = e.target;
+    setOnChagneSearchContent(value);
+    setIsSearch(false);
+  };
+  const onClickList = (data) => {
+    setClickList(data);
+  };
+
+  useEffect(() => {
+    var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+    var markers = [];
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch(onSubmitSearchContent, placesSearchCB);
+
+    function placesSearchCB(data, status, pagination) {
+      if (status === kakao.maps.services.Status.OK) {
+        let bounds = new kakao.maps.LatLngBounds();
+        for (let i = 0; i < data.length; i++) {
+          displayMarker(data[i]);
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        map.setBounds(bounds);
+        setPlaces(data);
+      }
+    }
+
+    function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x),
+      });
+
+      kakao.maps.event.addListener(marker, 'mouseover', function () {
+        infowindow.setContent(
+          '<span style="padding:3px;font-size:12px;display:inline-block;width:102%;">' + place.place_name + '</span>',
+        );
+        infowindow.open(map, marker);
+      });
+    }
+  }, [onSubmitSearchContent]);
+
+  return (
+    <S.MainSection>
+      <S.SearchListWrapper>
+        <SearchForm
+          onSubmitEvent={onSubmitEvent}
+          onChangeSearch={onChangeSearch}
+          value={onChagneSearchContent}
+        ></SearchForm>
+        <S.SearchList>
+          {places.map((item, i) => {
+            return (
+              <S.SearchListItem key={i}>
+                <div id="location-image"></div>
+                <div>
+                  <h1>{item.place_name}</h1>
+                  {item.road_adress_name ? <p>{item.road_address_name}</p> : <p>{item.address_name}</p>}
+                </div>
+              </S.SearchListItem>
+            );
+          })}
+        </S.SearchList>
+      </S.SearchListWrapper>
+      <S.KakaoMapWrapper>
+        <div id="designBox">도착지 검색</div>
+        <div id="map"></div>
+      </S.KakaoMapWrapper>
+    </S.MainSection>
+  );
+};
+
+export default SearchListMap;
---
+++ src/components/TaxiPotDetailView/SearchKakaoMap/SearchListMap/style.js
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import location_on_bar from '../../../../assets/images/location_on_bar.svg';
+
+export const MainSection = styled.section`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const SearchListWrapper = styled.div`
+  width: 810px;
+  height: 630px;
+  background-color: white;
+  border-radius: 26px;
+  margin-top: 59px;
+  box-shadow: 0 3px 20px rgba(0, 0, 0, 16%);
+`;
+
+export const KakaoMapWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 740px;
+  height: 690px;
+  #designBox {
+    width: 235px;
+    height: 60px;
+    background-color: #ffc044;
+    border-top-left-radius: 41px;
+    font-family: 'DoHyeon';
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-self: flex-end;
+  }
+  #map   {
+    width: 100%;
+    height: 630px;
+    border-radius: 17px;
+    border-top-right-radius: 0;
+  }
+`;
+
+export const SearchList = styled.div`
+  width: 85%;
+  height: 450px;
+  background-color: white;
+  border-radius: 5px;
+  margin-left: 65px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 16%);
+  overflow: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    width: 6px;
+  }
+  &::-webkit-scrollbar-thumb {
+    /* 스크롤바 막대 높이 설정 */
+    height: 17%;
+    background-color: #ffc044;
+    /* 스크롤바 둥글게 설정 */
+    border-radius: 10px;
+  } /* 스크롤바 뒷 배경 설정*/
+  &::-webkit-scrollbar-track {
+    background-color: rgba(0, 0, 0, 0);
+  }
+`;
+
+export const SearchListItem = styled.div`
+  width: 100%;
+  height: 112px;
+  border-bottom: #d4d4d4 solid 2px;
+  display: flex;
+  align-items: center;
+  #location-image {
+    background-image: url(${location_on_bar});
+    background-size: cover;
+    object-fit: cover;
+    width: 23px;
+    height: 36px;
+    margin-left: 28px;
+  }
+  div:nth-child(2) {
+    margin-left: 18px;
+  }
+  div:nth-child(2) > h1 {
+    font-size: 28px;
+    font-family: 'NotoSansCJK';
+  }
+  div:nth-child(2) > p {
+    font-family: 'NotoSansCJK';
+    font-size: 18px;
+    color: #8d8d8d;
+  }
+`;
