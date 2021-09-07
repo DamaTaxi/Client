a3c460af65aff1a4ede5e856f2e9494b7b6578a8
JaeHyun <soehd0889@naver.com>
2021-09-07 19:43:41 +0900

    [FEAT] <src/components/~/SearchKakaoMap> props code and func

--- src/components/TaxiPotDetailView/SearchKakaoMap/SearchKakaoMap.js
+++ src/components/TaxiPotDetailView/SearchKakaoMap/SearchKakaoMap.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './style';
 import { Link } from 'react-router-dom';
 import OptionWrapper from '../../../templates/OptionWrapper/OptionWrapper';
 import SearchListMap from './SearchListMap/SearchListMap';
 
 const SearchKakaoMap = () => {
+  const [onChagneSearchContent, setOnChagneSearchContent] = useState('');
+  const [onSubmitSearchContent, setOnSubmitSearchContent] = useState('');
+  const [clickPlaceData, setClickPlaceData] = useState([]);
+  const [currentItemIndex, setCurrentItemIndex] = useState(null);
+
+  useEffect(() => {
+    console.log(clickPlaceData);
+  }, [clickPlaceData]);
+
+  const onSubmitEvent = (e) => {
+    e.preventDefault();
+    setOnSubmitSearchContent(onChagneSearchContent);
+    setOnChagneSearchContent('');
+    setCurrentItemIndex(null);
+  };
+  const onChangeSearch = (e) => {
+    const { value } = e.target;
+    setOnChagneSearchContent(value);
+  };
+  const onClickList = (data, index) => {
+    setClickPlaceData(data);
+    setCurrentItemIndex(index);
+  };
   return (
     <S.SearchKakaoMapWrapper>
       <Link to="/" id="taxiPotLogoA">
         <S.TaxiPotLogo />
       </Link>
       <S.MainArticle>
-        <SearchListMap/>
-        <OptionWrapper contents="정확한 도착지를 찾아 선택해주세요!" buttonName="선택" cansleName="취소" link="#" />
+        <SearchListMap
+          onClickList={onClickList}
+          onChangeSearch={onChangeSearch}
+          onSubmitEvent={onSubmitEvent}
+          currentItemIndex={currentItemIndex}
+          clickPlaceData={clickPlaceData}
+          onSubmitSearchContent={onSubmitSearchContent}
+        />
+        <OptionWrapper
+          contents="정확한 도착지를 찾아 선택해주세요!"
+          buttonName="선택"
+          cansleName="취소"
+          cansleLink="/modify-my-taxi-pot"
+          successLink="/modify-my-taxi-pot"
+          clickPlaceData={clickPlaceData}
+          data={clickPlaceData}
+        />
+        {/* contents, buttonName, cansleName, cansleLink, successLink, onClickEvent */}
       </S.MainArticle>
       <S.BackgroundColor />
     </S.SearchKakaoMapWrapper>
