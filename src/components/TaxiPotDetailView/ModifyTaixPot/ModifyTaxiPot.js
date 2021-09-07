44bf86a3c7e338328bf0230aac8b8f8fda45f61a
JaeHyun <soehd0889@naver.com>
2021-09-07 19:45:14 +0900

    [FEAT] <src/~/ModifyTaxiPot.js> input value place연결

--- src/components/TaxiPotDetailView/ModifyTaixPot/ModifyTaxiPot.js
+++ src/components/TaxiPotDetailView/ModifyTaixPot/ModifyTaxiPot.js
@@ -3,11 +3,12 @@ import * as S from './style';
 import { Link } from 'react-router-dom';
 import OptionWrapper from '../../../templates/OptionWrapper/OptionWrapper';
 
-const ModifyTaxiPot = () => {
+const ModifyTaxiPot = (props) => {
+  const { data } = props.location.state;
   const test = (e) => {
     e.preventDefault();
-    console.log(e);
   };
+
   return (
     <S.TaxiPotWrapper>
       <Link to="/" id="taxiPotLogoA">
@@ -22,8 +23,14 @@ const ModifyTaxiPot = () => {
             <S.SearchContainer>
               <h1>도착지(제목)</h1>
               <div className="searchWrapper">
-                <input className="searchBar" type="text" value={'검색 버튼을 이용해 주세요'} />
-                <Link to="/taxi-pot">
+                <input
+                  className="searchBar"
+                  type="text"
+                  placeholder={'검색 버튼을 이용해 주세요.'}
+                  readOnly
+                  value={typeof data.place_name !== undefined ? data.place_name : '검색 버튼을 이용해 주세요.'}
+                />
+                <Link to="/search-kakao">
                   <button>검색</button>
                 </Link>
               </div>
