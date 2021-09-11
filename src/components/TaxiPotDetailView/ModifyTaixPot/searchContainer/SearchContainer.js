80bdee108f77824405df86cf62dabbc78354415b
JaeHyun <soehd0889@naver.com>
2021-09-11 16:39:27 +0900

    [ADD] <src/components/SearchContainer.js> 컴포넌트화

---
+++ src/components/TaxiPotDetailView/ModifyTaixPot/searchContainer/SearchContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as S from './style';
+import { Link } from 'react-router-dom';
+
+const SearchContainer = (props) => {
+  const { data } = props;
+  return (
+    <S.SearchContainer>
+      <h1>도착지(제목)</h1>
+      <div className="searchWrapper">
+        <input
+          className="searchBar"
+          type="text"
+          placeholder={'검색 버튼을 이용해 주세요.'}
+          readOnly
+          value={typeof data != 'undefined' ? data.data.place_name : '검색 버튼을 이용해 주세요.'}
+        />
+        <Link to="/search-kakao">
+          <button>검색</button>
+        </Link>
+      </div>
+    </S.SearchContainer>
+  );
+};
+
+export default SearchContainer;
