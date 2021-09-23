f20a79c257fa6faa95ec2d423535741ff897c0b1
JaeHyun <soehd0889@naver.com>
2021-09-23 20:07:51 +0900

    [ADD] 기능에 따른 history사용과 link태그 사용

--- src/templates/OptionWrapper/OptionWrapper.js
+++ src/templates/OptionWrapper/OptionWrapper.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import * as S from './style';
 
 const OptionWrapper = (props) => {
   const { contents, buttonName, cansleName, cansleLink, successLink, data } = props;
+  const history = useHistory();
+
+  const handleOnClickEvent = () => {
+    if (successLink !== '') history.push({ pathname: `${successLink}`, state: { data: data } });
+  };
+  function text() {
+    return;
+  }
+
   return (
     <S.OptionWrapper>
       <p>{contents}</p>
       <div id="buttonWrapper">
-        <Link
-          to={{
-            pathname: `${successLink}`,
-            state: {
-              data: data,
-            },
-          }}
-        >
-          <input type="submit" form="taxiPotForm" value={buttonName} />
-        </Link>
+        <input
+          type="submit"
+          onClick={typeof successLink !== 'undefined' ? handleOnClickEvent : text}
+          value={buttonName}
+        />
         <Link to={cansleLink}>
           <button>{cansleName}</button>
         </Link>
