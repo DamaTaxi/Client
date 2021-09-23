dda42de03c727897947ca5eaf9b386e82c902839
JaeHyun <soehd0889@naver.com>
2021-09-23 20:06:35 +0900

    [ADD] 데이터 로컬스토리지에 저장 기능

--- src/components/TaxiPotDetailView/ModifyTaixPot/searchContainer/SearchContainer.js
+++ src/components/TaxiPotDetailView/ModifyTaixPot/searchContainer/SearchContainer.js
@@ -3,7 +3,11 @@ import * as S from './style';
 import { Link } from 'react-router-dom';
 
 const SearchContainer = (props) => {
-  const { data, id, onChangeEvent, path } = props;
+  const { data, id, onChangeEvent, path, meetingAtData, myPageModifyData } = props;
+  const handleOnClickEvent = () => {
+    localStorage.setItem('meetingAtData', JSON.stringify(meetingAtData));
+    localStorage.setItem('myPageModifyData', JSON.stringify(myPageModifyData));
+  };
   return (
     <S.SearchContainer>
       <h1>도착지(제목)</h1>
@@ -18,7 +22,7 @@ const SearchContainer = (props) => {
           value={typeof data != 'undefined' ? data.data.place_name : '검색 버튼을 이용해 주세요.'}
         />
         <Link to={{ pathname: `/search-kakao:${id}`, state: { path: path } }}>
-          <button>검색</button>
+          <button onClick={handleOnClickEvent}>검색</button>
         </Link>
       </div>
     </S.SearchContainer>
