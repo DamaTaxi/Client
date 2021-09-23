e9cc4471c27720a54b7c870f1b95872cb1af98f9
JaeHyun <soehd0889@naver.com>
2021-09-23 20:06:17 +0900

    [ADD] formData axios데이터, link 태그 대신 history로 변환

--- src/components/TaxiPotDetailView/ModifyTaixPot/ModifyTaxiPot.js
+++ src/components/TaxiPotDetailView/ModifyTaixPot/ModifyTaxiPot.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './style';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useHistory } from 'react-router-dom';
 import OptionWrapper from '../../../templates/OptionWrapper/OptionWrapper';
 import RadioWrapper from './radioWrapper/RadioWrapper';
 import SearchContainer from './searchContainer/SearchContainer';
+import { requestWithAccessToken } from '../../../lib/axios';
 
-const ModifyTaxiPot = () => {
-  const [myPageModifyData, setMyPageModifyData] = useState({});
+const ModifyTaxiPot = (props) => {
+  const { id } = props;
+  const [myPageModifyData, setMyPageModifyData] = useState({
+    amount: '',
+    target: '',
+    place: '',
+    content: '',
+    latitude: '',
+    longitude: '',
+    title: '',
+    address: '',
+  });
+
+  const [meetingAtData, setMeetingAtData] = useState({});
+
+  const history = useHistory();
   const location = useLocation();
   const data = location.state;
 
@@ -14,7 +29,7 @@ const ModifyTaxiPot = () => {
     if (typeof location.state !== 'undefined') {
       setMyPageModifyData((prevState) => ({
         ...prevState,
-        adress: data.data.address_name,
+        address: data.data.address_name,
         title: data.data.place_name,
         latitude: data.data.y,
         longitude: data.data.x,
@@ -25,28 +40,60 @@ const ModifyTaxiPot = () => {
   useEffect(() => {
     setMyPageModifyData((prevState) => ({
       ...prevState,
+      meetingAt: `${meetingAtData.meetingAt_date}-${meetingAtData.meetingAt_time}`,
+    }));
+  }, [meetingAtData]);
+
+  useEffect(() => {
+    setMyPageModifyData((prevState) => ({
+      ...prevState,
       amount: 1,
     }));
   }, []);
 
-  console.log(data);
-  console.log(myPageModifyData);
+  const onChangeEvent = (e) => {
+    const { name, value } = e.target;
+    if (name === 'meetingAt_date' || name === 'meetingAt_time') {
+      setMeetingAtData((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    } else {
+      setMyPageModifyData((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
 
   const onKeyPressEvent = (e) => {
     if (e.key == 'Enter') e.preventDefault();
   };
 
-  const onChangeEvent = (e) => {
-    const { name, value } = e.target;
-    setMyPageModifyData((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+  const blankCheck = (values) => {
+    return values.some((ele) => {
+      if (ele === '') {
+        alert('빈칸이 있는 지 확인해주세요.');
+      }
+      return ele === '';
+    });
   };
 
-  const test = (e) => {
+  const handleSubmitEvent = (e) => {
     e.preventDefault();
-    console.log(myPageModifyData);
+    const ModifyData = Object.values(myPageModifyData);
+    if (blankCheck(ModifyData)) return;
+    requestWithAccessToken('patch', `/taxi-pot/${id}`, {}, myPageModifyData)
+      .then((res) => {
+        alert('택시팟이 수정되었습니다.');
+        setTimeout(() => {
+          history.push('/my-taxi-pot');
+        }, 1000);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
@@ -58,7 +105,7 @@ const ModifyTaxiPot = () => {
         <div className="modifyButton">
           <p>택시 팟 수정하기</p>
         </div>
-        <S.TaxiPotForm id="taxiPotForm" name="myForm" onSubmit={test}>
+        <S.TaxiPotForm id="taxiPotForm" name="myForm" onSubmit={handleSubmitEvent}>
           <S.FirstFloorContainer>
             <SearchContainer data={data} onChangeEvent={onChangeEvent} path="/modify-my-taxi-pot" id={1} />
             <S.PromisePlaceContainer>
@@ -81,22 +128,21 @@ const ModifyTaxiPot = () => {
             </div>
             <div className="dayAndTime" onKeyPress={onKeyPressEvent}>
               <h1>날짜와 시간</h1>
-              <input type="date" name="meetingAt-date" onChange={onChangeEvent} />
-              <input type="time" name="meetingAt-time" onChange={onChangeEvent} />
+              <input type="date" name="meetingAt_date" onChange={onChangeEvent} />
+              <input type="time" name="meetingAt_time" onChange={onChangeEvent} />
             </div>
           </S.SecondFloorContainer>
           <S.ThirdFloorContainer>
             <h1>상세설명</h1>
             <textarea name="content" onChange={onChangeEvent}></textarea>
           </S.ThirdFloorContainer>
+          <OptionWrapper
+            contents="변경사항이 있다면 수정완료 버튼을 눌러 팟을 수정해 주세요!"
+            buttonName="수정 완료"
+            cansleName="취소"
+            cansleLink="/my-taxi-pot"
+          />
         </S.TaxiPotForm>
-        <OptionWrapper
-          contents="변경사항이 있다면 수정완료 버튼을 눌러 팟을 수정해 주세요!"
-          buttonName="수정 완료"
-          cansleName="취소"
-          successLink="/my-taxi-pot"
-          cansleLink="/my-taxi-pot"
-        />
       </S.TaxiPotArticle>
       <S.BackgroundColor />
     </S.TaxiPotWrapper>
