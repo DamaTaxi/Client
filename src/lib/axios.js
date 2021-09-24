9cb75c16084cdc396058a539b4a12727cb47ee83
JaeHyun <soehd0889@naver.com>
2021-09-24 22:55:20 +0900

    [ADD] axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
+const ACCESS_TOKEN_NAME = 'Authorization';
 
 export const request = (method, url, headers, data) => {
   return axios({
@@ -17,18 +18,22 @@ export const request = (method, url, headers, data) => {
     });
 };
 
-export const requestWithAccessToken = (method, url, headers, data) => {
-  const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken');
-  return axios({
-    method,
-    url: BASE_URL + url,
-    headers: { ...headers, Authorization: ACCESS_TOKEN },
-    data,
-  })
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      throw err.response;
+export const requestWithAccessToken = async (url, method, headers, data) => {
+  /* const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken'); */
+  console.log(url, method, data, headers);
+  const ACCESS_TOKEN =
+    'Bearer ' +
+    'eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MzI0OTg2MDYsImlhdCI6MTYzMjQ5MTQwNiwic3ViIjoiaGVsbG9oZWxsb0BnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIn0.QZGWa6Bhc-GlRHpY_7m3Q2tdf_A-2d3qDCLtXjDqWMeNoXI6zQ7ft0_K64tctlu3JUHzstVVTIWuIpd3zdNVFw';
+  console.log(ACCESS_TOKEN);
+  try {
+    const res = await axios({
+      url: BASE_URL + url,
+      method,
+      headers: { ...headers, [ACCESS_TOKEN_NAME]: ACCESS_TOKEN },
+      data,
     });
+    return res.data;
+  } catch (err) {
+    throw err.response;
+  }
 };
