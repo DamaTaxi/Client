fea4b38ff99e35416f6fec9edfacf4f22e83339a
JaeHyun <soehd0889@naver.com>
2021-09-28 15:09:16 +0900

    [ADD] API 변경으로 인한 값변경

--- src/components/TaxiPot/TaxiPotList/TaxiPotList.js
+++ src/components/TaxiPot/TaxiPotList/TaxiPotList.js
@@ -13,26 +13,7 @@ const taxiPotDataArr = [
     creator: '김재현',
     target: 'ALL',
     createdAt: '2021-03-15-00:00',
-  },
-  {
-    title: '둔산동 꿀잼동전노래연습장',
-    meetingAt: '2021-12-03-00:00',
-    place: '기숙사 정문',
-    all: 4,
-    reserve: 3,
-    creator: '김재현',
-    target: 'ALL',
-    createdAt: '2021-03-15-00:00',
-  },
-  {
-    title: '둔산동 꿀잼동전노래연습장',
-    meetingAt: '2021-12-03-00:00',
-    place: '기숙사 정문',
-    all: 4,
-    reserve: 3,
-    creator: '김재현',
-    target: 'ALL',
-    createdAt: '2021-03-15-00:00',
+    adress: '대전중구 유천1동',
   },
 ];
 
@@ -43,15 +24,10 @@ function dateSplit(string) {
   return date;
 }
 
-//3자리수 마다 콤마 찍어주는 함수
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
-
 const TaxiPotList = () => {
   const taxiPotListItem = taxiPotDataArr.length
     ? taxiPotDataArr.map((taxiPotDataArr, index) => {
-        const { title, meetingAt, place, all, reserve, createdAt, target, creator } = taxiPotDataArr;
+        const { title, meetingAt, place, all, reserve, createdAt, target, creator, adress } = taxiPotDataArr;
         return (
           <Link to="/taxi-pot" key={index}>
             <S.TaxiPotList>
@@ -61,13 +37,13 @@ const TaxiPotList = () => {
               </div>
               <div className="list-section-wrapper">
                 <S.FirstFloorWrapper>
-                  <p id="marginRight" >모집자: {creator}</p>
+                  <p id="marginRight">모집자: {creator}</p>
                   <GraphContainer reserve={reserve} all={all} width={250} left={44} height={17} marginTop={5} />
                   <p>약속장소 : {place}</p>
                 </S.FirstFloorWrapper>
                 <S.SecondFloorWrapper>
                   <p>대상자 : {target}</p>
-                  <p>거리 : 9.8km</p>
+                  <p>주소 : {adress}</p>
                   <p>날짜와 시간: {meetingAt}</p>
                 </S.SecondFloorWrapper>
               </div>
@@ -82,7 +58,7 @@ const TaxiPotList = () => {
       <>
         <S.ListHeader>
           <h1>택시 팟 리스트</h1>
-          <Link to="#">
+          <Link to="/make-taxi-pot">
             <p>+</p>
             <p>택시 팟 만들기</p>
           </Link>
