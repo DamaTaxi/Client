12d025cc1d65a6947fcbb60214f9939a912c5751
JaeHyun <soehd0889@naver.com>
2021-09-29 14:23:56 +0900

    .

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -20,7 +20,7 @@ export const request = (method, url, headers, data) => {
 export const requestWithAccessToken = (method, url, headers, data) => {
   const ACCESS_TOKEN =
     'Bearer ' +
-    'eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MzI4Mjk5ODUsImlhdCI6MTYzMjgyMjc4NSwic3ViIjoiaGVsbG9oZWxsb0BnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIn0.ptxhIXaKVuGaqUGjknfuIXGbjay4s2IYVNE3R9Cs7q0z9NkhQ58v9lhmHjp_dI0KTYceGsZ9NKf8TJOue0mD1w';
+    'eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MzI4Nzc1MTQsImlhdCI6MTYzMjg3MDMxNCwic3ViIjoiaGVsbG9oZWxsb0BnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIn0.6ODoFljh_Vhx96lU0SFseXZg3sh5UbjouxyKe08BbAooH9NPjwi68POvBSV4wcfUbTLEl1_xMlFbkcnVqFlQVQ';
   /* const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken'); */
   return axios({
     method,
