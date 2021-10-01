6dd4f11a95b03348c73c80dbf0f200373ced62ed
seonghyeon <shp35079@naver.com>
2021-10-01 22:43:11 +0900

    [ADD] ListModal api 연동

--- src/Modal/ListModal/ListModal.js
+++ src/Modal/ListModal/ListModal.js
@@ -7,11 +7,8 @@ const ListModal = ({ ModalContent, isShowModal, closeModal }) => {
       <>
         <S.Background onClick={closeModal} />
         <S.ModalContainer>
-          <span>택시팟이 안 만들어져요 ㅠㅠ</span>
-          <S.ContentContainer>
-            입력칸 다 채우고 생성 버튼 눌렀는데도 제가 만든 팟이 안 보이네요 ㅠㅠ 오류 고쳐주세요!! 저 제주도
-            가야합니다.
-          </S.ContentContainer>
+          <span>{ModalContent.title}</span>
+          <S.ContentContainer>{ModalContent.content}</S.ContentContainer>
           <S.ButtonContainer>
             <button onClick={closeModal}>확인</button>
             <button>삭제</button>
