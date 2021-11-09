8a5274bd6a4a5d12a32c3a777dbfb653106cbc14
seonghyeon <shp35079@naver.com>
2021-11-10 08:46:53 +0900

    [FIX] axios 파일 수정

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -36,7 +36,7 @@ export const refresh = () => {
     });
 };
 
-/* export const requestWithAccessToken = (method, url, headers, data) => {
+export const requestWithAccessToken = (method, url, headers, data) => {
   const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken');
   return axios({
     method,
@@ -55,24 +55,5 @@ export const refresh = () => {
         default:
           throw err;
       }
-    });
-}; */
-
-export const requestWithAccessToken = (method, url, headers, data) => {
-  const ACCESS_TOKEN =
-    'Bearer ' +
-    'eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MzY0NzE4NzUsImlhdCI6MTYzNjQ2NDY3NSwic3ViIjoiaGVsbG9oZWxsb0BnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIn0.fgHrYXHwn_OstjL-nVi_kqHfd2osNU-gVfbO0a-gZZwED6FyiutXGJkmcKxHjn8Glf_v9eCj_fRlmK0Xf5X_uw';
-  /* const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken'); */
-  return axios({
-    method,
-    url: BASE_URL + url,
-    headers: { ...headers, Authorization: ACCESS_TOKEN },
-    data,
-  })
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      throw err;
     });
 };
