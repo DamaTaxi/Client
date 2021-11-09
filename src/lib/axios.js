48c0d1dcdb75a245d959088c5b26bbab33f584e7
JaeHyun <soehd0889@naver.com>
2021-11-09 23:15:58 +0900

    .

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -36,7 +36,7 @@ export const refresh = () => {
     });
 };
 
-export const requestWithAccessToken = (method, url, headers, data) => {
+/* export const requestWithAccessToken = (method, url, headers, data) => {
   const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken');
   return axios({
     method,
@@ -55,5 +55,24 @@ export const requestWithAccessToken = (method, url, headers, data) => {
         default:
           throw err;
       }
+    });
+}; */
+
+export const requestWithAccessToken = (method, url, headers, data) => {
+  const ACCESS_TOKEN =
+    'Bearer ' +
+    'eyJhbGciOiJIUzUxMiJ9.eyJleHAiOjE2MzY0NzE4NzUsImlhdCI6MTYzNjQ2NDY3NSwic3ViIjoiaGVsbG9oZWxsb0BnbWFpbC5jb20iLCJ0eXBlIjoiYWNjZXNzIn0.fgHrYXHwn_OstjL-nVi_kqHfd2osNU-gVfbO0a-gZZwED6FyiutXGJkmcKxHjn8Glf_v9eCj_fRlmK0Xf5X_uw';
+  /* const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken'); */
+  return axios({
+    method,
+    url: BASE_URL + url,
+    headers: { ...headers, Authorization: ACCESS_TOKEN },
+    data,
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw err;
     });
 };
