ba9da4ece61a1b9146cbaac41339f8a70bbafc6d
seonghyeon <shp35079@naver.com>
2021-11-11 22:06:52 +0900

    [CREATE] axios 파일추가

---
+++ src/lib/axios.js
@@ -0,0 +1,118 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const request = (method, url, headers, data) => {
+  return axios({
+    method,
+    url: BASE_URL + url,
+    headers,
+    data,
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw err.response;
+    });
+};
+
+export const refresh = () => {
+  const refresh = localStorage.getItem('refreshToken');
+  request('patch', '/token-refresh', {}, { refreshToken: refresh })
+    .then((res) => {
+      localStorage.setItem('accessToken', res.data['accessToken']);
+      localStorage.setItem('refreshToken', res.data['refreshToken']);
+    })
+    .catch((err) => {
+      switch (err.response.status) {
+        case 401:
+          localStorage.setItem('isCheckLogin', false);
+          window.location.href = '/admin-main';
+          break;
+        default:
+          throw err.response;
+      }
+    });
+};
+
+export const requestWithAccessToken = (method, url, headers, data) => {
+  const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken');
+  return axios({
+    method,
+    url: BASE_URL + url,
+    headers: { ...headers, Authorization: ACCESS_TOKEN },
+    data,
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      switch (err.response.status) {
+        // case 401:
+        //   refresh();
+        //   break;
+        default:
+          throw err.response;
+      }
+    });
+};
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+export const request = (method, url, headers, data) => {
+  return axios({
+    method,
+    url: BASE_URL + url,
+    headers,
+    data,
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw err.response;
+    });
+};
+
+export const refresh = () => {
+  const refresh = localStorage.getItem('refreshToken');
+  request('patch', '/token-refresh', {}, { refreshToken: refresh })
+    .then((res) => {
+      localStorage.setItem('accessToken', res.data['accessToken']);
+      localStorage.setItem('refreshToken', res.data['refreshToken']);
+    })
+    .catch((err) => {
+      switch (err.response.status) {
+        case 401:
+          localStorage.setItem('isCheckLogin', false);
+          window.location.href = '/admin-main';
+          break;
+        default:
+          throw err.response;
+      }
+    });
+};
+
+export const requestWithAccessToken = (method, url, headers, data) => {
+  const ACCESS_TOKEN = 'Bearer ' + localStorage.getItem('accessToken');
+  return axios({
+    method,
+    url: BASE_URL + url,
+    headers: { ...headers, Authorization: ACCESS_TOKEN },
+    data,
+  })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      switch (err.response.status) {
+        // case 401:
+        //   refresh();
+        //   break;
+        default:
+          throw err.response;
+      }
+    });
+};
