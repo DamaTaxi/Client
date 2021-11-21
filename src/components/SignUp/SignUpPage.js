bb2b98d9ab30199717000c55ce4d812fdf9863f2
JaeHyun <soehd0889@naver.com>
2021-11-21 14:07:54 +0900

    [ADD] sign-up 기능 구현

--- src/components/SignUp/SignUpPage.js
+++ src/components/SignUp/SignUpPage.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as S from './style';
 import headerLogoYellow from '../../assets/images/TaxiPot/header-logo-yellow.png';
 import SearchContainer from '../TaxiPotDetailView/ModifyTaixPot/searchContainer/SearchContainer';
-import { useLocation } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
+import { requestWithAccessToken } from '../../lib/axios';
 
 const SignUpPage = () => {
   const [inputData, setInputData] = useState('');
 
+  const history = useHistory();
   const location = useLocation();
   const data = location.state;
+
+  console.log(inputData);
 
   const blankCheck = (values) => {
     return values.some((ele) => {
@@ -23,22 +27,40 @@ const SignUpPage = () => {
     setInputData(e.target.value);
   };
 
+  const onSubmitEvent = () => {
+    const { x, y, address_name } = data.data;
+    let blankCheckData = { x, y, address_name, inputData };
+    const checkData = Object.values(blankCheckData);
+    if (blankCheck(checkData)) return;
+    requestWithAccessToken('patch', `/mypage`, {}, { tel: inputData, latitude: x, longitude: y, address: address_name })
+      .then((res) => {
+        setTimeout(() => {
+          history.push('/user-main');
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <S.SignUpWrapper>
       <S.SignUpContainer>
         <img src={headerLogoYellow} alt="로고 이미지" />
         <h1>대마택시에 오신 것을 환영합니다.</h1>
-        <S.InputWrapper>
-          <p style={{ marginTop: '80px' }}>전화번호를 입력해 주세요</p>
-          <input type="text" placeholder="전화번호를 입력해 주세요" onChange={OnChangeEvent} />
-        </S.InputWrapper>
         <S.SearchContainer>
           <p>자주가는 목적지를 검색해 주세요.</p>
           <SearchContainer id={100} path="/sign-up" data={data} isSignUp={true} />
           <span>주소 : {data && data.data.address_name}</span>
           <span>관련 : {data && data.data.category_name}</span>
         </S.SearchContainer>
-        <button className="submit">시작하기</button>
+        <S.InputWrapper>
+          <p style={{ marginTop: '80px' }}>전화번호를 입력해 주세요</p>
+          <input type="text" placeholder="전화번호를 입력해 주세요" onChange={OnChangeEvent} />
+        </S.InputWrapper>
+        <button className="submit" onClick={onSubmitEvent}>
+          시작하기
+        </button>
       </S.SignUpContainer>
     </S.SignUpWrapper>
   );
